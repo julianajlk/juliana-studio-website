6200bc3888f29fa2ef92408e782ff24d6bdffaf6
JULIANAJLK <JULIANAJLK@GMAIL.COM>
2015-09-12 21:36:05 -0400

    add js nav bar

--- js/main.js
+++ js/main.js
@@ -8,5 +8,52 @@ $(window).on('load', function(){
 });
 
 
+$(function() {
+  // mobile-menu
+  $('a.menu-icon').click(function() {
+    $('.nav-2').slideToggle();
+    $('body').css({'overflow':'hidden'});
+    $('#social').show();
+    $('.nav-3').css({
+      'display': 'block',
+      'position': 'absolute',
+      'width': '100%',
+      'text-align': 'center',
+      'left': '0'
+    });
+  });
+
+  $('.menu-close').click(function(){
+    $('.nav-2').hide();
+    $('body').removeAttr('style');
+    $('#social').removeAttr('style');
+    $('.nav-3').removeAttr('style');
+  });
+
+  // fix hidden links on window resize
+
+  $(window).resize(function() {
+    if ($(window).width() > 900) {
+      $('.nav-2').removeAttr('style');
+      $('body').removeAttr('style');
+      $('#social').removeAttr('style');
+      $('.nav-3').removeAttr('style');
+    }
+  });
+
+  $(window).bind('scroll', function() {
+     var navHeight = $( window ).height() - 70;
+       if ($(window).scrollTop() > navHeight) {
+         $('nav').addClass('fixed');
+       }
+       else {
+         $('nav').removeClass('fixed');
+       }
+    });
+
+});
+
+
+
 
 
